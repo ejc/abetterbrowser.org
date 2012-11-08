724485f540f03837abcf44ade0b42ceb26098432
ejc <ej@ejc.me>
2012-11-08 11:16:36 -0800

    create some partial views; new credits page (too many people to thank); remove safari (no pc support from apple on v6); fix up some of the translation files (still needs more work)

--- abb.rb
+++ abb.rb
@@ -5,14 +5,29 @@ require 'sinatra'
 require 'sinatra/r18n'
 require 'haml'
 
+helpers do
+  def partial(page, options={})
+    haml page.to_sym, options.merge!(:layout => false)
+  end
+end
+
 before do
   session[:locale] = params[:locale] if params[:locale]
 end
  
 get '/' do
+    @locale = "en"
     haml :index
+
 end
 
 get '/:locale' do
+    @locale = params[:locale]
     haml :index
+   
 end
+
+get '/:locale/credits' do
+    haml :credits
+end
+
